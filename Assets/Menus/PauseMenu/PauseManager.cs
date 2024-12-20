616b5c828468093d430d662d52d0d69baec744ff
Sergi Rebollo <78222988+giseer@users.noreply.github.com>
2024-12-20 14:12:22 +0100

    Endless Mode, MainMenu Remake
    I created the endless mode with no time and i remake main menu, when you press play appear other buttons to choose the game mode and i change the font style of splash company to take coherency

--- Assets/Menus/PauseMenu/PauseManager.cs
+++ Assets/Menus/PauseMenu/PauseManager.cs
@@ -1,75 +1,13 @@
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
+    [SerializeField] private GameObject pauseCanvas;
 
-    public static PauseManager Instance { get; private set; }
-
-    private void Awake() 
-    { 
-        if (Instance != null && Instance != this) 
-        { 
-            Destroy(this); 
-        } 
-        else 
-        { 
-            Instance = this; 
-        }
-
-        for (int i = 0; i < SceneManager.sceneCount; i++)
-        {
-            if (SceneManager.GetSceneAt(i).name == "PauseMenu")
-            {
-                NavigatorManager.UnloadScene("PauseMenu");
-            }
-        }
-    }
-    
-    [SerializeField] private InputActionReference pause;
-
-    public UnityEvent ResumedGame;
-    
-    private bool isPaused = false;
-
-    private void OnEnable()
+    public void TogglePauseCanvas()
     {
-        pause.action.Enable();
-    }
+        pauseCanvas.SetActive(!pauseCanvas.activeSelf);
 
-    void Update()
-    {
-        if (pause.action.WasPerformedThisFrame())
-        {
-            if (isPaused)
-            {
-                ResumeGame();
-            }
-            else
-            {
-                PauseGame();    
-            }
-            isPaused =!isPaused;
-        }
-    }
-
-    private void PauseGame()
-    {
-        Time.timeScale = 0f;
-        SceneManager.LoadSceneAsync("PauseMenu", LoadSceneMode.Additive);
-    }
-    
-    private void ResumeGame()
-    {
-        Time.timeScale = 1f;
-        SceneManager.UnloadSceneAsync("PauseMenu");
-        ResumedGame.Invoke();
-    }
-
-    private void OnDisable()
-    {
-        pause.action.Disable();
+        Time.timeScale = pauseCanvas.activeSelf ? 0 : 1;
     }
 }
